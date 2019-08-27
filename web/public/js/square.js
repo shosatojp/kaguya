6dfcce76ec96cfd2130550177f20ea6fff205979
shosatojp <me@shosato.jp>
2019-08-27 16:01:59 +0900

    page

--- web/public/js/square.js
+++ web/public/js/square.js
@@ -1,6 +1,6 @@
 const RandSquare = (function () {
-    const RandSquare = function (x, y) {
-        this.x = x; this.y = y;
+    const RandSquare = function (x, y, title) {
+        this.x = x; this.y = y; this.title = title;
         this.square = this._gen_rand_square(x, y);
     };
     const STRING_SRC = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&\'()=-~^|\\[{]}:*@`;+_/?.>,<';
@@ -16,7 +16,7 @@ const RandSquare = (function () {
         }
         return result;
     };
-    RandSquare.prototype.create_table = function (title = undefined) {
+    RandSquare.prototype.create_table = function () {
         const wrap = document.createElement('div');
         wrap.classList.add('square_wrap');
         const table = document.createElement('table');
@@ -32,28 +32,47 @@ const RandSquare = (function () {
         }
         wrap.appendChild(table);
         const e_title = document.createElement('div');
-        e_title.textContent = title || '';
+        e_title.textContent = this.title || '';
         e_title.classList.add('square_title');
         wrap.appendChild(e_title);
         return wrap;
     };
-    RandSquare.prototype.create_canvas = function (cw = 500, ch = 500, title = undefined, color = '#000') {
-        const title_h = 50;
+    RandSquare.prototype.create_canvas = function (cw = 500, ch = 500, color = '#000') {
         const canvas = document.createElement('canvas');
         canvas.height = ch, canvas.width = cw;
-        const h = canvas.height - (!!title * title_h), w = canvas.width;
+        this.draw_on_canvas(canvas, cw, ch, 0, 0, color);
+    };
+    RandSquare.prototype.draw_on_canvas = function (canvas, sw = 500, sh = 500, origin_x = 0, origin_y = 0, color = '#000') {
+        const title_h = 50;
+        const h = sh - (!!this.title * title_h), w = sw;
         const context = canvas.getContext('2d');
         const text_h = h / this.y, text_w = w / this.x;
         context.font = Math.min(text_h, text_w) * 0.7 + 'px monospace';
         context.fillStyle = color;
         context.textBaseline = 'center';
         context.textAlign = 'center';
+        context.rect(origin_x, origin_y, sw, sh);
+        context.stroke();
         for (let i = 0; i < this.y; i++) {
             for (let j = 0; j < this.x; j++) {
-                context.fillText(this.square[i][j], j * text_w + text_w / 2, i * text_h + text_h / 2);
+                context.fillText(this.square[i][j], origin_x + j * text_w + text_w / 2, origin_y + 5 + i * text_h + text_h / 2);
             }
         }
-        if (title) context.fillText(title, w / 2, canvas.height - title_h / 2);
+        if (this.title) context.fillText(this.title, origin_x + w / 2, origin_y + sh - title_h / 2);
+    };
+    RandSquare.create_document = function (randsquares = [], squaresize = 500, col = 3, margin = 10) {
+        const canvas = document.createElement('canvas');
+        canvas.height = Math.ceil(randsquares.length / col) * squaresize + margin * 2;
+        canvas.width = col * squaresize + margin * 2;
+        let x = margin, y = margin;
+        randsquares.forEach((rs, i) => {
+            rs.draw_on_canvas(canvas, squaresize, squaresize, x, y);
+            if ((i + 1) % col === 0)
+                y += squaresize, x = margin;
+            else
+                x += squaresize;
+        });
+        return canvas;
     };
     return RandSquare;
 })();
